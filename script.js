ecb95ef9a0774119cf5a2fce51ebeb49a22a4c89
Yan Pablo <93152439+YanppSilva@users.noreply.github.com>
2023-09-12 16:58:58 -0300

    Add files via upload
    Validação do Form da sessão Contato.

--- script.js
+++ script.js
@@ -85,4 +85,67 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const numeroRegex = /^[0-9]{11}$/;
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  nameValidate();
+  emailValidate();
+  numberValidate();
+  subjectValidate();
+  textValidate()
+}); 
+
+function setError(index) {
+  campos[index].style.border = '2px solid #e63636';
+  spans[index].style.display = 'block';
+}
+
+function removeError(index) {
+  campos[index].style.border = '';
+  spans[index].style.display = 'none';
+}
+
+function nameValidate() {
+  if(campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0)
+  }
+}
+
+function emailValidate() {
+  if(!emailRegex.test(campos[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
+}
+
+function numberValidate() {
+  if(!numeroRegex.test(campos[2].value)) {
+    setError(2);
+  } else {
+    removeError(2);
+  }
+}
+
+function subjectValidate() {
+  if(campos[3].value.length < 1) {
+    setError(3);
+  } else {
+    removeError(3);
+  }
+}
+
+function textValidate() {
+  if(campos[4].value.length < 1) {
+    setError(4);
+  } else {
+    removeError(4);
+  }
+}
